25ded11ba27f3b6e64dde8fbcaf49331a3b309a3
Fauzan Zaman <fauzanzaman000@gmail.com>
2025-11-25 15:26:32 +0700

    fix: cors

--- src/utils/apiClients.ts
+++ src/utils/apiClients.ts
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 const apiSquare = axios.create({
   baseURL: import.meta.env.VITE_API_WEB_BASE_URL as string,
-  headers: {
-    'x-api-key': import.meta.env.VITE_API_KEY_SQUARE as string,
-  },
 });
 
 
