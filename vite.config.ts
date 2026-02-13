72aaf3b1a5d0cff1d2dd93b761682bdd9f89ea0b
Fauzan Zaman <fauzanzaman000@gmail.com>
2026-02-13 14:25:46 +0700

    add imagemin plugin to compress images

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,46 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { VitePluginRadar, } from 'vite-plugin-radar'
+import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
-  plugins: [react(),
-  VitePluginRadar({
-    enableDev: true,
-    gtm: [
-      {
-        id: 'GTM-W94TJ64',
-      }
-    ],
-  }),
+  plugins: [
+    react(),
+    VitePluginRadar({
+      enableDev: true,
+      gtm: [
+        {
+          id: 'GTM-W94TJ64',
+        }
+      ],
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 75,
+      },
+      pngquant: {
+        quality: [0.7, 0.8],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
   ],
   build: {
     chunkSizeWarningLimit: 1600
